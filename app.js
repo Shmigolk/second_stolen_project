c218ab88a9d5e8b6c147b83cd66438140bcb9c94
Shmigolk <shmigolftf@gmail.com>
2022-03-28 13:34:24 +0300

    Final commit

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const closeBtn = document.querySelector('.close-btn');
 const sidebar = document.querySelector('.sidebar');
 
 toggleBtn.addEventListener('click', () => {
-    if (sidebar.classList.contains('show-sidebar')){
+    /*if (sidebar.classList.contains('show-sidebar')){
         sidebar.classList.remove('show-sidebar');
     }
     else {
         sidebar.classList.add('show-sidebar');
-    }
+    }*/
+    sidebar.classList.toggle('show-sidebar');
+})
+
+closeBtn.addEventListener('click', () => {
+    sidebar.classList.remove('show-sidebar');
 })
